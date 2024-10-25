87192500e0757b6c57142e010651ca580e4ea4e3
Magno Chiabai <magnochiabai@gmail.com>
2024-10-25 16:34:47 -0300

    fix(tag): corrige métodos de API e códigos de status * Adicionado seção sobre utilização do Swagger na documentação

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -3,12 +3,14 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
   Delete,
   UseInterceptors,
   UploadedFile,
   NotFoundException,
+  Put,
+  HttpCode,
+  HttpStatus,
 } from '@nestjs/common';
 import { TagService } from './tag.service';
 import { UpdateTagDto } from './dto/update-tag.dto';
@@ -19,29 +21,41 @@ import { Tag } from './entities/tag.entity';
 @ApiTags('tags')
 @Controller('tag')
 export class TagController {
-  constructor(private readonly tagService: TagService) { }
+  constructor(private readonly tagService: TagService) {}
 
   @Post('upload')
+  @HttpCode(HttpStatus.OK)
   @UseInterceptors(FileInterceptor('file'))
   @ApiOperation({ summary: 'Enviar arquivo excel com as tags' })
-  @ApiResponse({ status: 201, description: 'Arquivo processado com sucesso' })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'Arquivo processado com sucesso',
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'Arquivo inválido ou formato incorreto',
+  })
   async uploadFile(@UploadedFile() file: any) {
     await this.tagService.processExcelFile(file);
     return { message: 'Arquivo processado com sucesso.' };
   }
 
   @Get()
+  @HttpCode(HttpStatus.OK)
   @ApiOperation({ summary: 'Buscar todas as tags' })
-  @ApiResponse({ status: 200, type: [Tag] })
+  @ApiResponse({ status: HttpStatus.OK, type: [Tag] })
   findAll() {
-    console.log('loading');
     return this.tagService.findAll();
   }
 
   @Get(':tag')
+  @HttpCode(HttpStatus.OK)
   @ApiOperation({ summary: 'Buscar tag por ID' })
-  @ApiResponse({ status: 200, type: Tag })
-  @ApiResponse({ status: 404, description: 'Tag nao encontrada' })
+  @ApiResponse({ status: HttpStatus.OK, type: Tag })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'Tag nao encontrada',
+  })
   findOne(@Param('tag') tag: string): Tag {
     const foundTag = this.tagService.findOne(tag);
     if (!foundTag) {
@@ -50,18 +64,33 @@ export class TagController {
     return foundTag;
   }
 
-  @Patch(':tag')
+  @Put(':tag')
+  @HttpCode(HttpStatus.OK)
   @ApiOperation({ summary: 'Atualizar uma tag' })
-  @ApiResponse({ status: 200, type: Tag })
-  @ApiResponse({ status: 404, description: 'Tag nao encontrada' })
+  @ApiResponse({ status: HttpStatus.OK, type: Tag })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Tag nao encontrada',
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'Dados inválidos',
+  })
   update(@Param('tag') tag: string, @Body() updateTagDto: UpdateTagDto) {
     return this.tagService.update(tag, updateTagDto);
   }
 
   @Delete(':tag')
+  @HttpCode(HttpStatus.NO_CONTENT)
   @ApiOperation({ summary: 'Deletar tag' })
-  @ApiResponse({ status: 200, description: 'Tag removida com sucesso' })
-  @ApiResponse({ status: 404, description: 'Tag nao encontrada' })
+  @ApiResponse({
+    status: HttpStatus.NO_CONTENT,
+    description: 'Tag removida com sucesso',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Tag nao encontrada',
+  })
   remove(@Param('tag') tag: string) {
     const deleted = this.tagService.delete(tag);
     if (!deleted) {
